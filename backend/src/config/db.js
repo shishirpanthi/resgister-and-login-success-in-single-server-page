d2018dacafb0d5f192f7a4c178fbe8d17b152d25
shishir panthi <shishir12312@gmail.com>
2025-04-20 16:08:02 +0545

    making folder structure for frontend and backend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config({ path: "./src/.env" });
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI); // Debug log
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
